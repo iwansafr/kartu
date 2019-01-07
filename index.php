6dac1058a5b109a68b08a94124912b63f4f4ec72
iwansafr <iwansafr@gmail.com>
2019-01-07 08:34:47 +0700

    mastering

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>aplikasi kartu</h1>
+				<form action="" method="post">
+					<div class="form-group">
+						<label for="tahun">tahun</label>
+						<select name="tahun" class="form-control">
+							<option value="2019">2019</option>
+						</select>
+					</div>
+					<div class="form-group">
+						<label for="judul">judul</label>
+						<input type="text" name="judul" class="form-control">
+					</div>
+					<div class="form-group">
+						<input type="submit" class="btn btn-success" value="submit">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript" src="assets/jquery/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
